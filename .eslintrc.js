9a9378f9c13a89be6b49b12d62a4d35982afc316
Gonzan <gonzalovanni@gmail.com>
2023-07-22 11:52:46 +0200

    initial config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+      es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['react', 'react-hooks'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  };
+  
