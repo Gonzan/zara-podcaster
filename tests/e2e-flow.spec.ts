1cca591745b5d767bbd30940328cf8e263600110
Gonzan <gonzalovanni@gmail.com>
2023-07-24 22:22:40 +0200

    fix test

--- tests/e2e-flow.spec.ts
+++ tests/e2e-flow.spec.ts
@@ -2,16 +2,22 @@ import { test, expect } from '@playwright/test';
 
 test('Elements visibillity flow', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  expect(await page.getByRole('link', { name: 'Podcaster' })).toBeVisible();
-  expect(await page.getByTestId('badge')).toHaveText('100');
-  expect(await page.getByTestId('search-section')).toBeVisible();
+  await expect(await page.getByRole('link', { name: 'Podcaster' })).toBeVisible();
+  await expect(await page.getByTestId('badge')).toHaveText('100');
+  await expect(await page.getByTestId('search-section')).toBeVisible();
 
   await page.locator('a').filter({ hasText: 'The Joe Budden PodcastAuthor: The Joe Budden Network' }).click();
-  expect(page.getByRole('link', { name: 'podcast image' })).toBeVisible();
-  expect(page.getByRole('link', { name: 'The Joe Budden Podcast By The Joe Budden Network' }))
-  expect(page.getByText('Episodes')).toBeVisible();
-  expect(page.getByTestId('podcastList')).toBeVisible();
-  await page.getByRole('link', { name: 'Episode 642 | "That Energy"' }).click();
-  expect(page.getByRole('link', { name: 'podcast image' })).toBeVisible();
-  await page.getByRole('link', { name: 'podcast image' }).click();
+  await expect(page.getByRole('link', { name: 'The Joe Budden Podcast By The Joe Budden Network' })).toBeVisible({
+    timeout: 1000,
+  });
+  await expect(page.getByText('Episodes: 20')).toBeVisible({
+    timeout: 1000,
+  });
+  await expect(page.getByTestId('podcastList')).toBeVisible();
+  await page.getByRole('link', { name: 'Episode 642 | "That Energy"' }).click({
+    timeout: 1000,
+  });
+
+  await page.getByRole('link', { name: 'The Joe Budden Podcast By The Joe Budden Network' }).click()
+  await expect(await page.getByRole('link', { name: 'Podcaster' })).toBeVisible();
 });
